16722e89858dcdb4d0b2994dd8e6debe73e8997d
Syed <Syed@edt.srv01.in>
2025-09-10 16:45:15 +0530

    this is endpoint for loading attendance

--- routers/biometricattendance/biometric.attendance.route.js
+++ routers/biometricattendance/biometric.attendance.route.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { addBiometricDevice, fetchAttendance, getBiometricDevice } from '../../controllers/biometricattendance/biometric.attendance.controller.js';
+import { addBiometricDevice, fetchAttendance, getBiometricDevice,getAttendanceFromDevice } from '../../controllers/biometricattendance/biometric.attendance.controller.js';
 import { authenticate } from '../../helpers/authenticateEmployee.js';
 
 const biometricRouter = express.Router();
 
   
 
-biometricRouter.post('/attendance',authenticate, fetchAttendance).post('/addbiometricdevice', authenticate , addBiometricDevice).get('/getbiometricdevice', authenticate, getBiometricDevice);
+biometricRouter.post('/attendance',authenticate, fetchAttendance).post('/addbiometricdevice', authenticate , addBiometricDevice).get('/getbiometricdevice', authenticate, getBiometricDevice).post('/loadAttendance',getAttendanceFromDevice);
 
 
 export default biometricRouter;
